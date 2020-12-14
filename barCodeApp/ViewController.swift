6a02fd721886dbfb6b37144ffaacb02f4e482301
Shohei Shikanai <shikanai1993@gmail.com>
2020-12-14 21:50:17 +0900

    Initial Commit

---
+++ barCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  barCodeApp
+//
+//  Created by 鹿内翔平 on 2020/12/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
